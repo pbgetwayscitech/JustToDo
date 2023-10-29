5a3973ac03d4ef022c12e66528238c0992a6a713
pbgetwayscitech <pb.this@outlook.com>
2023-10-29 10:17:55 +0530

    build 02

--- app/src/main/java/com/pb/ria/new_and_edit_task.kt
+++ app/src/main/java/com/pb/ria/new_and_edit_task.kt
@@ -7,138 +7,118 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.room.Room
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.pb.ria.db.ndb
 import com.pb.ria.db.nen
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
 class new_and_edit_task : AppCompatActivity() {
-
-    lateinit var title_text: TextInputEditText
-    lateinit var desccription_text : TextInputEditText
-    lateinit var save : Button
-    lateinit var done : Button
-    lateinit var delete : Button
-    var key : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_and_edit_task)
-
-        title_text = findViewById(R.id.task_edittext_title)
-        desccription_text = findViewById(R.id.task_edittext_description)
-        save = findViewById(R.id.task_btton_save)
-        done = findViewById(R.id.task_button_done)
-        delete = findViewById(R.id.task_button_delete)
 
         load_if_saved()
 
     }
 
     fun load_if_saved(){
-        val input_title_key = intent.getStringExtra("title_key")+""
-        if(!input_title_key.trim().toString().equals("")){
-            // check_in_sql_database(input_title_key!!)
-
-            val note_db  =  Room.databaseBuilder(this,
-                ndb::class.java,
-                "notedb").build()
-            //val all = note_db.notedao().get_all_notes()
-
-            key = input_title_key
-        }else{
-            setonclicklistner()
-        }
-    }
-
-    fun check_in_sql_database( title_key : String){
-        print("justtodo _checking in sql database")
-        check_sql_database(this, this,title_key).start()
-    }
-
-    fun setonclicklistner(){
-        print("justtodo _setting up onclick listeners")
-
-        save.setOnClickListener {
-            inset_into_sql_database(this,this,title_text.text.toString(),
-                desccription_text.text.toString(),"","").start()
-        }
-
-        done.setOnClickListener {
-
-        }
-
-        delete.setOnClickListener {
-
-        }
-
+        val input_title_key = intent.getStringExtra("key")+""
+        manage_sql_database(this,this,input_title_key).start()
     }
 
 }
 
-class check_sql_database( mcontext : Context , mactivity : Activity , mkey : String) : Thread() {
+class manage_sql_database( mcontext : Context , mactivity : Activity , mkey : String) : Thread() {
 
     val activity  = mactivity
     val context  =  mcontext
     val key  = mkey
+    lateinit var title_text: TextInputEditText
+    lateinit var desccription_text : TextInputEditText
+    lateinit var save : Button
+    lateinit var done : Button
+    lateinit var delete : Button
 
     override fun run() {
         super.run()
 
-//        val note_db  =  Room.databaseBuilder(context, Note_database::class.java,const.db_name).build()
+        title_text = activity.findViewById(R.id.task_edittext_title)
+        desccription_text = activity.findViewById(R.id.task_edittext_description)
+        save = activity.findViewById(R.id.task_btton_save)
+        done = activity.findViewById(R.id.task_button_done)
+        delete = activity.findViewById(R.id.task_button_delete)
+        val note_db  =  Room.databaseBuilder(context, ndb::class.java, "notedb").build()
+        val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy/HH:mm:ss", Locale.getDefault())
+        val currentDateAndTime = simpleDateFormat.format(Date())
 
-        val mlist = emptyList<nen>() // note_db.notedao().search_in_notest(key)
-        if(mlist.isNotEmpty()){
-            print("justodo _one note found")
+        if(!key.trim().toString().equals("")){
+            // key is not ""
 
-            val index_zero = mlist.get(0)
-            activity.runOnUiThread {
-                val title_text  : TextInputEditText = activity.findViewById(R.id.task_edittext_title)
-                val desccription_text  : TextInputEditText = activity.findViewById(R.id.task_edittext_description)
+            title_text.setText(note_db.ndao().search_in_notest(key).get(0).Title)
+            desccription_text.setText(note_db.ndao().search_in_notest(key).get(0).Description)
 
-                title_text.setText(index_zero.Title)
-                desccription_text.setText(index_zero.Description)
+            save.setOnClickListener {
+
+                val mnen = nen()
+                mnen.id = note_db.ndao().search_in_notest(key).get(0).id
+                mnen.Title = title_text.text.toString()
+                mnen.Description = desccription_text.text.toString()
+                mnen.Timestamp = currentDateAndTime.toString()
+                mnen.state = "saved"
+                note_db.ndao().update_into_database(mnen)
+
             }
+
+            done.setOnClickListener {
+
+                val mnen = nen()
+                mnen.id = note_db.ndao().search_in_notest(key).get(0).id
+                mnen.Title = title_text.text.toString()
+                mnen.Description = desccription_text.text.toString()
+                mnen.Timestamp = currentDateAndTime.toString()
+                mnen.state = "compleated"
+                note_db.ndao().update_into_database(mnen)
+
+            }
+
+            delete.setOnClickListener {
+                note_db.ndao().deletenote(key)
+                Snackbar.make(context,save,"Note Deleted !",Snackbar.LENGTH_LONG).show()
+                activity.finish()
+            }
+
         }else{
-            activity.finish()
-        }
-    }
-}
+            // key is ""
+            save.setOnClickListener {
 
-class inset_into_sql_database ( mcontext: Context,
-                                mactivity: Activity,mtitle : String ,
-                                mdesc : String,
-                                mkey :String,
-                                maction: String,) : Thread(){
+                val mnen = nen()
+                mnen.Title = title_text.text.toString()
+                mnen.Description = desccription_text.toString()
+                mnen.Timestamp = currentDateAndTime
+                mnen.state = "saved"
+                note_db.ndao().insert_note(mnen)
 
-    val activity = mactivity
-    val title  = mtitle
-    val context = mcontext
-    val desc = mdesc
-    val key =  mkey
-    val action  = maction
+            }
 
-   // val note_db  =  Room.databaseBuilder(context, Note_database::class.java,const.db_name).build()
+            done.setOnClickListener {
 
-    override fun run() {
-        super.run()
+                val mnen = nen()
+                mnen.Title = title_text.text.toString()
+                mnen.Description = desccription_text.toString()
+                mnen.Timestamp = currentDateAndTime
+                mnen.state = "compleated"
+                note_db.ndao().insert_note(mnen)
+            }
 
-        /*
-
-        if (action == const.note_state.save){
-
-           // val single_note = single_note_entity("",title,desc,Date().toString(),const.note_state.save.toString())
-           // note_db.notedao().insert_into_note(single_note);
-
+            delete.setOnClickListener {
+                Snackbar.make(context,save,"This note has not been created yet",Snackbar.LENGTH_LONG).show()
+                activity.finish()
+            }
         }
 
-        if (action == const.note_state.update){
-
-           // val single_note = single_note_entity("",title,desc,Date().toString(),const.note_state.save.toString())
-           // note_db.notedao().update_into_database(single_note)
-
-        }
-
-         */
     }
 }
