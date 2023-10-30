1b946246584751c44a75cf726ed9cbeb6998362e
pbgetwayscitech <pb.this@outlook.com>
2023-10-30 21:50:45 +0530

    VER 6 PLAY-STORE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pb.ria"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 6
+        versionName = "6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
